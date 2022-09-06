bdb80814eb496f30e19987d6f7f86bf2bac10141
EraserandRain <1349291258@qq.com>
2022-09-06 18:33:09 +0800

    perf:update scripts in zsh/

--- zsh/update_local.sh
+++ zsh/update_local.sh
@@ -1,4 +1,46 @@
-#!/usr/bin/env bash
-cat ~/install/zsh/bak_for_zshrc > ~/.zshrc
-source ~/.zshrc
+#!/usr/bin/env zsh
+ZSHFILE=
+print_usage() {
+    cat <<EOF
+    
+    Usage: $0 [OPTIONS]    
+
+    OPTIONS:
+        -w : update local ~/.zshrc in WSL
+        -u : update local ~/.zshrc in Ubuntu-Docker
+        -h : show this help and exit    
+
+EOF
+    exit 1
+}
+
+update_wsl() {
+    $ZSHFILE=wsl
+    cat ~/install/zsh/bak_for_zshrc/$ZSHFILE >~/.zshrc
+    source ~/.zshrc
+    exit 0
+}
+
+update_ubuntu() {
+    $ZSHFILE=ubuntu_docker
+    cat ~/install/zsh/bak_for_zshrc/$ZSHFILE >~/.zshrc
+    source ~/.zshrc
+    exit 0
+}
+
+# Main
+while getopts "h w u" OPT
+do
+    case $OPT in
+        h)
+            print_usage
+            ;;
+        w)
+            update_wsl
+            ;;
+        u)
+            update_ubuntu
+            ;;
+    esac
+done
 exit 0
