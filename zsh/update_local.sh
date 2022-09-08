2f9a5dac6a62f2c5b3e9806ce27870e1ecb1ab6d
EraserandRain <1349291258@qq.com>
2022-09-08 12:25:09 +0800

    feat: update zsh/bak_for_zshrc

--- zsh/update_local.sh
+++ zsh/update_local.sh
@@ -1,45 +1,48 @@
 #!/usr/bin/env zsh
+SCRIPTFILE=$0
 ZSHFILE=
 print_usage() {
     cat <<EOF
     
-    Usage: $0 [OPTIONS]    
+    Usage: $SCRIPTFILE [OPTIONS]    
 
     OPTIONS:
         -w : update local ~/.zshrc in WSL
-        -u : update local ~/.zshrc in Ubuntu-Docker
+        -d : update local ~/.zshrc in Docker
+        -r : update local ~/.zshrc in Remote Server
         -h : show this help and exit    
 
 EOF
     exit 1
 }
 
-update_wsl() {
-    ZSHFILE=wsl
-    cat ~/install/zsh/bak_for_zshrc/$ZSHFILE > ~/.zshrc
-    source ~/.zshrc
-    exit 0
-}
-
-update_ubuntu() {
-    ZSHFILE=ubuntu_docker
+update_local_zsh() {
     cat ~/install/zsh/bak_for_zshrc/$ZSHFILE > ~/.zshrc
     source ~/.zshrc
     exit 0
 }
 
 # Main
-while getopts "h w u" OPT
+while getopts "h w d r" OPT
 do
     case $OPT in
         h)
             print_usage
             ;;
         w)
-            update_wsl
+            ZSHFILE=wsl
+            update_local_zsh
             ;;
-        u)
-            update_ubuntu
+        d)
+            ZSHFILE=Docker
+            update_local_zsh
+            ;;
+        r)
+            ZSHFILE=Remote
+            update_local_zsh
+            ;;
+        *)
+            print_usage
             ;;
     esac
 done
