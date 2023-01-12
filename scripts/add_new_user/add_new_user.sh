aa868d85588c9ede3148b5fe7e3b54469d6947e6
EraserandRain <1349291258@qq.com>
2023-01-12 18:25:49 +0800

    fix: fix some syntax error in scripts/add_new_user

--- scripts/add_new_user/add_new_user.sh
+++ scripts/add_new_user/add_new_user.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 NEW_USER='eraserrain'
 NEW_GROUPS='eraserrain'
 PASS_WORD='Abcd1234'
@@ -9,3 +10,4 @@ if [[ $? -ne 0 ]]; then
     useradd ${NEW_USER} -s /bin/bash -g ${NEW_GROUPS} -G sudo -d "/home/${NEW_USER}" -m
     echo ${NEW_USER}:${PASS_WORD} | chpasswd
     bash -c "echo '${NEW_USER} ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/${NEW_USER}"
+fi
