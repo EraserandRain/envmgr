20b519b9d0a6411404fabe29c16b7cdbd78e346b
EraserandRain <1349291258@qq.com>
2023-03-23 14:54:26 +0800

    fix: init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+export ENV_ROOT_DIR="env-manager"
+source ${HOME}/${ENV_ROOT_DIR}/PROJECT_ENV
+source ${ENV_ROOT}/include/common.sh
+
 is_cmd_exist zsh && load_env --zsh $(hostname -s) || install_env --zsh
 is_cmd_exist nvm node npm nrm pnpm && load_env --node || install_env --node 
 is_cmd_exist gvm go && load_env --golang || install_env --golang
